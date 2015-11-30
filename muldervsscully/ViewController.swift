374fe4c7f0a517b2fb19b1de24a14dbee399bafc
Jason King <threecolorcar@Jasons-MacBook-Pro.local>
2015-11-29 16:02:12 -0800

    adding files for test

--- muldervsscully/ViewController.swift
+++ muldervsscully/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mulder: UIImageView!
+    @IBOutlet weak var scully: UIImageView!
+    @IBOutlet weak var mulderbutton: UIButton!
+    @IBOutlet weak var scullybutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func scullybuttonpress(sender: AnyObject) {
+        scully.hidden=false
+        mulder.hidden=true
+    }
 
+    @IBAction func mulderbuttonpress(sender: AnyObject) {
+        scully.hidden=true
+        mulder.hidden=false
+    }
 }
 
